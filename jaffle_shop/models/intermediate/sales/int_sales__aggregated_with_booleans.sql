53a7b09813fe5b2d2b9ba6fe43ea40950c22ba29
Rafael Jürkfitz <rafaeljurkfitz@hotmail.com>
2024-11-12 21:52:27 -0300

    fix: fix int

--- jaffle_shop/models/intermediate/sales/int_sales__aggregated_with_booleans.sql
+++ jaffle_shop/models/intermediate/sales/int_sales__aggregated_with_booleans.sql
@@ -4,8 +4,31 @@ orders as (
     select * from {{ ref('stg_jaffle_shop__orders') }}
 ),
 
+order_items as (
+    select * from {{ ref('order_items') }}
+),
+
 order_items_summary as (
-    select * from {{ ref('int_sales__items_summary') }}
+
+    select
+
+        order_id,
+
+        sum(supply_cost) as order_cost,
+        sum(product_price) as order_items_subtotal,
+        count(order_item_id) as count_order_items,
+
+        sum(
+            case when is_food_item then 1 else 0 end
+        ) as count_food_items,
+        sum(
+            case when is_drink_item then 1 else 0 end
+        ) as count_drink_items
+
+    from order_items
+
+    group by 1
+
 ),
 
 aggregated as (
